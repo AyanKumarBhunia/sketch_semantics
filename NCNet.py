Fixed masking.

NCNet.py
@@ -1,63 +1,34 @@
+import pdb
 import torch.nn as nn
 import torch
-# import pdb
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 import torch.nn.functional as F
 
+
 def max_neg_value(tensor):
     return -torch.finfo(tensor.dtype).max
 
-def corr_score_us(corr_xy, len_A, len_B):
-    corr_B = corr_xy  # .permute(0, 2, 1)
-    corr_A = corr_xy.permute(0, 2, 1)
 
-    # score_A, _ = corr_A.softmax(dim=-1).max(-1)   # (b, N1, N2).max(2) -> (b, N1, 1)
-    # score_B, _ = corr_B.softmax(dim=-1).max(-1)   # (b, N2, N1).max(2) -> (b, N2, 1)
+def corr_score(corr_xy, len_A, len_B):
+    corr_A = corr_xy  # .permute(0, 2, 1)
+    corr_B = corr_xy.permute(0, 2, 1)
 
     score_A_mean = 0
-    for x, y, z in zip(corr_B, len_A, len_B):
+    for x, y, z in zip(corr_A, len_A, len_B):
         score_A_ab = x[:y, :z]
-        print(torch.isinf(score_A_ab).sum())
+        if (torch.isinf(score_A_ab).sum()) !=0:
+            pdb.set_trace()
         score_A_mean += F.softmax(score_A_ab, dim=-1).max(-1)[0].mean()
 
     score_B_mean = 0
-    for x, y, z in zip(corr_A, len_B, len_A):
+    for x, y, z in zip(corr_B, len_B, len_A):
         score_B_ab = x[:y, :z]
-        print(torch.isinf(score_B_ab).sum())
+        if (torch.isinf(score_B_ab).sum()) != 0:
+            pdb.set_trace()
         score_B_mean += F.softmax(score_B_ab, dim=-1).max(-1)[0].mean()
 
     score = (score_A_mean + score_B_mean) / 2
-    print(score)
-
-    # corr_B: batch, N1_max, N2_max
-    return score
-
-
-def corr_score(corr_xy):
-    corr_B = corr_xy  # .permute(0, 2, 1)
-    corr_A = corr_xy.permute(0, 2, 1)
-
-    corr_A.softmax(dim=-1)
-
-    score_A, _ = corr_A.softmax(dim=-1).max(-1)   # (b, N1, N2).max(2) -> (b, N1, 1)
-    score_B, _ = corr_B.softmax(dim=-1).max(-1)   # (b, N2, N1).max(2) -> (b, N2, 1)
-
-
-    print(torch.nansum(score_A), torch.nansum(score_B))
-    # score_A_mean = torch.nansum(score_A)/ (1 - torch.isnan(score_A).float()).sum()
-    # score_B_mean = torch.nansum(score_B) / (1 - torch.isnan(score_B).float()).sum()
-
-
-    score_A_mean = (torch.where(torch.isnan(score_A), torch.tensor(0.0).to(device), score_A)).sum() / (1 - torch.isnan(score_A).float()).sum()
-    score_B_mean = (torch.where(torch.isnan(score_B), torch.tensor(0.0).to(device), score_B)).sum() / (1 - torch.isnan(score_B).float()).sum()
-
-    # https://discuss.pytorch.org/t/torch-nansum-yields-nan-gradients-unexpectedly/117115/3
-
-    print(score_A_mean, score_B_mean)
-    score = (score_A_mean + score_B_mean) / 2
-
-    print(score)
     return score
 
 
@@ -74,21 +45,10 @@ class FeatureCorrelation(torch.nn.Module):
         self.normalization = normalization
 
     def forward(self, feature_A, feature_B, mask_A, mask_B):
-        assert(len(feature_A.shape) == 3)
-        assert(len(feature_B.shape) == 3)
-
-        # taking 3D version of NCNets
-        b, N1, d = feature_A.size()
-        b, N2, d = feature_B.size()
-        # reshape features for matrix multiplication
-        # (b, N2, d) -> (b, d, N2)
-        feature_B = feature_B.transpose(1, 2)
-        # feature_A = feature_A
-
         # perform matrix mult.
         # (b, N1, d) @ (b, d, N2) -> (b, N1, N2)
+        feature_B = feature_B.transpose(1, 2)
         correlation_tensor = torch.bmm(feature_A, feature_B)           # equation 1
-        # mask = max_neg_value(torch.bmm(mask_A, mask_B.transpose(1, 2)))
 
         if self.normalization:
             correlation_tensor = featureL2Norm(torch.nn.functional.relu(correlation_tensor))
@@ -96,14 +56,11 @@ class FeatureCorrelation(torch.nn.Module):
         return correlation_tensor
 
 
-def MutualMatching(corr2d):  #b, N1, N2 is input ..
+def MutualMatching(corr2d):  # corr2d = [b, N1, N2]
     # mutual matching
     # enforced reciprocity. given i-th row -- if argmin is j <===> given j-th col -- argmin is i
     # argmin of axis 2 w.rt. axis 1 should be equal to argmin of axis 1 w.r.t axis 2
 
-
-
-    # get max
     corr2d_N1_max, _ = torch.max(corr2d, dim=1, keepdim=True)    # along N1 axis
     corr2d_N2_max, _ = torch.max(corr2d, dim=2, keepdim=True)    # along N2 axis
 
@@ -111,39 +68,30 @@ def MutualMatching(corr2d):  #b, N1, N2 is input ..
     corr2d_N1 = corr2d / (corr2d_N1_max + eps)  # equation 5
     corr2d_N2 = corr2d / (corr2d_N2_max + eps)
 
-    # corr4d_B = corr4d_B.view(batch_size, 1, fs1, fs2, fs3, fs4)  # doesn't require realignment
-    # corr4d_A = corr4d_A.view(batch_size, 1, fs1, fs2, fs3, fs4)  # OLD 4D
-
     corr2d = corr2d * (corr2d_N1 * corr2d_N2)  # parenthesis are important for symmetric output  # equation 4
 
     return corr2d
 
 class NC_Conv2D_Masked(nn.Module):
-    def __init__(self, ch_hidden=10, k_size=3):
+    def __init__(self, ch_hidden=10, k_size=3, ):
         super(NC_Conv2D_Masked, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, out_channels=ch_hidden, kernel_size=k_size,
-                                bias=True, padding=1)
-        self.conv2 = nn.Conv2d(ch_hidden, out_channels=ch_hidden, kernel_size=k_size,
-                                bias=True, padding=1)
-        self.conv3 = nn.Conv2d(ch_hidden, out_channels=1, kernel_size=k_size,
-                                bias=True, padding=1)
+        self.conv1 = nn.Conv2d(1, out_channels=ch_hidden, kernel_size=k_size, bias=True, padding=1)
+        self.conv2 = nn.Conv2d(ch_hidden, out_channels=ch_hidden, kernel_size=k_size, bias=True, padding=1)
+        self.conv3 = nn.Conv2d(ch_hidden, out_channels=1, kernel_size=k_size, bias=True, padding=1)
         self.relu = nn.ReLU()
 
-    def forward(self, x):
-        mask_cal = torch.ones_like(x)
-        mask_cal[x == 0] = 0. # batch, 1, N1, N2
-
-        x = self.relu(self.conv1(x)) * mask_cal # batch, 10, N1, N2
-        x = self.relu(self.conv2(x)) * mask_cal
-        x = self.relu(self.conv3(x)) * mask_cal
+    def forward(self, x, corr_mask_conv):              # corr_mask_conv = batch, 1, N1, N2
+        x = self.relu(self.conv1(x)) * corr_mask_conv  # batch, 10, N1, N2
+        x = self.relu(self.conv2(x)) * corr_mask_conv
+        x = self.relu(self.conv3(x)) * corr_mask_conv
 
         return x
 
 
 class NeighbourhoodConsensus2D(nn.Module):
     def __init__(self, use_conv=True, pool=False, k_size=None, use_cuda=True,
-                 kernel_sizes=[3, 3, 3], channels=[10, 10, 1], symmetric_mode=False):
+                 kernel_sizes=[3, 3, 3], channels=[10, 10, 1], symmetric_mode=True):
         super(NeighbourhoodConsensus2D, self).__init__()
         self.symmetric_mode = symmetric_mode
         self.kernel_sizes = kernel_sizes
@@ -153,50 +101,36 @@ class NeighbourhoodConsensus2D(nn.Module):
         self.corr = FeatureCorrelation()
         self.use_conv = use_conv
 
-        # self.masked_conv = NC_Conv2D_Masked()
-        # if use_cuda:
-        #     self.masked_conv.cuda()
+        self.masked_conv = NC_Conv2D_Masked()
+        if use_cuda:
+            self.masked_conv.cuda()
 
-    def forward(self, feature_A, feature_B, mask_A, mask_B):
-        # feature_A -> b, N1, d
-        # feature_B -> b, N2, d
+    def forward(self, feature_A, feature_B, mask_A, mask_B):  # A-> b, N1, d ; B-> b, N2, d
 
-        corr_tensor = self.corr(feature_A, feature_B, mask_A, mask_B)      # equation 1  -- done
-        # corr_tensor -> b, N1, N2
-        # mask basically takes care of the negative infinity part.
+        corr_tensor = self.corr(feature_A, feature_B, mask_A, mask_B)      # equation 1   [b, N1, N2]
+        corr_mask = torch.bmm(mask_A,mask_B.permute(0, 2, 1))
+        corr_mask = corr_mask / 512.0                       # only 1s and 0s
+
         assert (torch.isfinite(corr_tensor).all())
-        # pdb.set_trace()
         corr_tensor = MutualMatching(corr_tensor)                               # equation 4+5
 
         corr_tensor = corr_tensor.unsqueeze(1)
-
-        # if self.symmetric_mode:                                                 # equation 2
-        #     # apply network on the input and its "transpose" (swapping A-B to B-A ordering of the correlation tensor),
-        #     # this second result is "transposed back" to the A-B ordering to match the first result and be able to add together
-        #     corr_tensor = self.masked_conv(corr_tensor) + self.masked_conv(corr_tensor.permute(0, 1, 3, 2)).permute(0, 1, 3, 2)
-        #     # because of the ReLU layers in between linear layers,
-        #     # this operation is different than convolution of a single time with the filters+filters^T
-        #     # and therefore it makes sense to do this.
-        # else:
-        #     corr_tensor = self.masked_conv(corr_tensor)
-
-
+        corr_mask = corr_mask.unsqueeze(1)
+        if self.symmetric_mode:
+            corr_tensor = self.masked_conv(corr_tensor, corr_mask) + \
+                self.masked_conv(corr_tensor.permute(0, 1, 3, 2), corr_mask.permute(0, 1, 3, 2)).permute(0, 1, 3, 2)
+        else:
+            corr_tensor = self.masked_conv(corr_tensor)
         corr_tensor = corr_tensor.squeeze(1)
+        corr_mask = corr_mask.squeeze(1)
 
-        corr_tensor = MutualMatching(corr_tensor)
+        corr_tensor = MutualMatching(corr_tensor)                               # equation 4+5
 
         # https://medium.com/analytics-vidhya/masking-in-transformers-self-attention-mechanism-bad3c9ec235c
         # MASKING to deal with softmax -- summation over softmax output = 1.
+        corr_mask = torch.where(corr_mask==1.0, torch.tensor(0.0).to(device), torch.tensor(-float('inf')).to(device))
+        corr_tensor = corr_tensor + corr_mask
 
-        mask = torch.zeros_like(corr_tensor)
-        mask[corr_tensor == 0] = -float('inf')
-        corr_tensor_masked = corr_tensor + mask
-
-        # mask_index = [torch.where(m==0)[0] for m in mask]
-
-        #  masking strategy 1: -float('inf') followed by softmax
-        #  masking strategy 1: float('nan') followed by torch.nansum()
-
-        return corr_tensor_masked
+        return corr_tensor
 
 

dataset.py
@@ -17,7 +17,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 unseen_classes = ['bat', 'cabin', 'cow', 'dolphin', 'door', 'giraffe', 'helicopter', 'mouse', 'pear', 'raccoon',
                   'rhinoceros', 'saw', 'scissors', 'seagull', 'skyscraper', 'songbird', 'sword', 'tree',
                   'wheelchair', 'windmill', 'window']
-# /vol/research/sketchCV/datasets/Sketchy
+# /vol/research/sketchCV/datasets/Sketchy/
 
 
 class Sketchy_Dataset(data.Dataset):
@@ -26,7 +26,7 @@ class Sketchy_Dataset(data.Dataset):
         self.hp = hp
         self.mode = mode
         self.training = copy.deepcopy(hp.training)
-        with open('sketchy_all.pickle', 'rb') as fp:
+        with open('/vol/research/sketchCV/datasets/Sketchy/sketchy_all.pickle', 'rb') as fp:
             train_sketch, test_sketch, self.negativeSampleDict, self.Coordinate = pickle.load(fp)
 
         set_A = [x for x in train_sketch if x.split('/')[0] not in unseen_classes]

main.py
@@ -42,7 +42,10 @@ if __name__ == "__main__":
         model = Sketch_Classification(hp)
         model.to(device)
         step = 0
-        best_test_loss = 100
+        best_test_loss = 10
+
+        with torch.no_grad():
+            best_test_loss = model.evaluate(dataloader_Test)
 
         for epoch in range(hp.max_epoch):
             for i_batch, batch in enumerate(dataloader_Train):
@@ -50,7 +53,7 @@ if __name__ == "__main__":
                 step += 1
 
                 if (step + 0) % hp.print_freq_iter == 0:
-                    print(f'Epoch: {epoch:0>5} \tIter: {i_batch:0>5} \tSteps: {step:0>5} \tLoss_NCN: {loss_ncn:.5f}')
+                    print(f'Epoch: {epoch:0>3} \tIter: {i_batch:0>5} \tSteps: {step:0>5} \tLoss_NCN: {loss_ncn:.5f}')
 
                 if (step + 1) % hp.eval_freq_iter == 0:
                     show(step, batch, model)
@@ -67,3 +70,4 @@ if __name__ == "__main__":
     #     print(message)
     #     if hp.disable_tqdm:
     #         client.send('saneeshan95@gmail.com', message)
+    #         client.send('2ajay.das@gmail.com', message)

model.py
@@ -23,7 +23,7 @@ class Sketch_Classification(nn.Module):
         self.Network = Stroke_Embedding_Network(hp)
         self.train_params = self.parameters()
         self.optimizer = optim.Adam(self.train_params, hp.learning_rate)
-        self.score = corr_score_us
+        self.score = corr_score
         self.hp = hp
         self.neighbour = NeighbourhoodConsensus2D(use_conv=hp.use_conv, pool=hp.pool, k_size=hp.k_size)
 
@@ -41,22 +41,35 @@ class Sketch_Classification(nn.Module):
         neg_score = self.score(corr_xneg, num_stroke_anc, num_stroke_neg)
 
         loss_ncn = neg_score - pos_score
-        return loss_ncn
+        return loss_ncn, corr_xpos
 
     def train_model(self, batch):
         self.train()
         self.optimizer.zero_grad()
-        loss = self.calc_loss(batch) # neg_score - pos_score
+        loss, _ = self.calc_loss(batch) # neg_score - pos_score
         loss.backward()
         self.optimizer.step()
         return loss.item()
 
-    def evaluate(self, dataloader_Test):
+    def evaluate_old(self, dataloader_Test):
         self.eval()
         test_loss = 0
         start_time = time.time()
         for i_batch, batch in enumerate(tqdm(dataloader_Test, desc='Testing', disable=self.hp.disable_tqdm)):
             test_loss += self.calc_loss(batch).item()
+
+        test_loss /= len(dataloader_Test.dataset)
+        print(f'\nTest set: Average loss: {test_loss:.5f}, Time_Takes: {(time.time() - start_time):.5f}\n')
+
+        return test_loss
+
+    def evaluate(self, dataloader_Test):    #  in Progress
+        self.eval()
+        test_loss = 0
+        start_time = time.time()
+        for i_batch, batch in enumerate(tqdm(dataloader_Test, desc='Testing', disable=self.hp.disable_tqdm)):
+            score_pos, corr_pos = self.calc_loss(batch)
+            test_loss += score_pos.item()      # this needs to change to hard match
 
         test_loss /= len(dataloader_Test.dataset)
         print(f'\nTest set: Average loss: {test_loss:.5f}, Time_Takes: {(time.time() - start_time):.5f}\n')

stroke_visualiser.py
@@ -4,26 +4,28 @@ import numpy as np
 from datetime import datetime
 from PIL import Image, ImageDraw
 from rasterize import rasterize_Sketch
+from model import make_mask
 
-time_id = datetime.now().strftime("%b%d_%H:%M:%S")
+time_id = datetime.now().strftime("%b%d_%H-%M-%S")
 
 
 def show(step, batch, model):
     if not os.path.isdir(time_id):
         os.makedirs(time_id)
+
     output_anc, num_stroke_anc = model.Network(batch, type='anchor')  # b,N1,512
     output_pos, num_stroke_pos = model.Network(batch, type='positive') # b,N2,512
+    mask_anc, mask_pos = map(make_mask, [num_stroke_anc, num_stroke_pos])
+    corr_xpos = model.neighbour(output_anc, output_pos, mask_anc, mask_pos)
 
     im = Image.new('RGB', (266*output_anc.shape[0], 5+261*2))     # width x height
     '''
     I need the n1 x n2 matrix
     For every element in N1, the top 2 values in N2 for which the N1xN2 matrix value is highest, will be coloured
     '''
-    for i_sample, sample_anc in enumerate(output_anc):
-        sample_pos = output_pos[i_sample]  # N2x512
-        A = torch.matmul(sample_anc, sample_pos.t())  # N1 x N2
-        maxA = A.max()
-        a,b = torch.where(A == maxA)
+    for i_sample, A in enumerate(corr_xpos):
+        a, b = torch.where(A == A.max())
+        a, b = a[0].item(), b[0].item()       # in the rare case there are multiple same max values
 
         k = batch['anchor_sketch_vector']
         anchor = Image.fromarray(255-rasterize_Sketch(k[i_sample].numpy())).convert('RGB')
@@ -39,7 +41,7 @@ def show(step, batch, model):
         start_index = sum(batch['num_stroke_per_positive'][:i_sample])
         beta_pos = batch['stroke_wise_split_positive'][start_index: start_index + nOFstroke].numpy()
 
-        a, b = a.item() % beta_anc.shape[0], b.item() % beta_pos.shape[0]    # not accurate - avoid errors
+        a, b = a % beta_anc.shape[0], b % beta_pos.shape[0]    # not accurate - avoid errors
 
         beta_stroke_points_anc = np.where(255 - rasterize_Sketch(beta_anc[a]) == 0)
         for i_point in range(len(beta_stroke_points_anc[0])):